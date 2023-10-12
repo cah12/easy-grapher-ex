3b67598d3e71104c9c4d4bf15d5a7f21c869ae89
Corwin <corwin_hope@yahoo.com>
2023-10-12 03:28:05 -0400

    Major bug fixes and code refactoring

--- wrapper.js
+++ wrapper.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 //Remember to set heroku env. variables for production.
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -5,21 +7,26 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
-const path = require("path");
 const app = express();
-const cookieParser = require("cookie-parser");
 
-app.use(cookieParser());
+const options = {
+  //white_list: ["http://127.0.0.1:5500", "http://localhost:3500/"], //default: undefined
+  //access_token_expiry: "15s", //default: 900s
+  // cookie_max_age: 1000 * 60 * 60, //default: 1000 * 60 * 60 * 24 (1 day)
+  //cookie_secure: false, //default: true
+};
 
+//Only needed for same domain (i.e. when no whitelist is provided in options argument).
+const { router, authenticateToken, getUserFs } = require("mongo-db-filesystem")(
+  app,
+  options
+);
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({ limit: "140mb", extended: true }));
-
-const fs = require("file-system-mongo");
-const fileSystemRoutes = require("file-system-routes")(fs);
-app.use(fileSystemRoutes);
 
 app.use(express.static(path.join(__dirname, "public", "www-built")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
-  console.log("Server started on PORT " + PORT);
+  console.log(`Server started on PORT http://localhost:${PORT}`);
 });
