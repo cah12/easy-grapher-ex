2aa5db7242fe838b4b19e7f340cb932f5e8fe22f
Corwin <corwin_hope@yahoo.com>
2024-02-05 21:44:51 -0500

    Major bug fixes and code refactoring

--- wrapper.js
+++ wrapper.js
@@ -13,8 +13,8 @@ const options = {
   white_list: [
     "http://127.0.0.1:5500",
     // "http://localhost:3500/",
-    //"https://cah12.github.io/easy-grapher",
-    "https://cah12.github.io",
+    "https://cah12.github.io/easy-grapher",
+    //"https://cah12.github.io",
   ], //default: undefined
   //access_token_expiry: "15s", //default: 900s
   // cookie_max_age: 1000 * 60 * 60, //default: 1000 * 60 * 60 * 24 (1 day)
